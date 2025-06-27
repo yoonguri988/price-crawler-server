ce415099b09933b978b4915e70d32d71c833b280
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-06-28 02:16:22 +0900

    [chore] Render 배포 위한 puppeteer 경로 설정 및 render.yaml 추가

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -4,8 +4,14 @@
 import puppeteer, { Browser } from "puppeteer";
 
 export const getBrowser = async (): Promise<Browser> => {
+  const isRender = process.env.RENDER === "true"; // 환경변수로 플랫폼 구분
+  const executablePath = isRender
+    ? "/opt/render/.cache/puppeteer/chrome/linux-*/chrome"
+    : puppeteer.executablePath();
+
   return await puppeteer.launch({
     headless: true,
+    executablePath,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 };
