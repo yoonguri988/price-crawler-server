5a3a9aefc058c8bbd3727bd885836b1ed4d67bdf
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-07-01 04:05:39 +0900

    chore: index.ts에 mockRouter 연결 (/api/mock)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 // src/index.ts
 import express from "express";
 import crawlRouter from "./routes/crawl.route";
+import mockRouter from "./routes/mock.route";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use("/api/crawl", crawlRouter);
+app.use("/api/mock", mockRouter);
 
 app.listen(PORT, () => {
   console.log(`✅ Server running at: ${PORT}`);
