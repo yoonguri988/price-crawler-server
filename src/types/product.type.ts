151a7ca58dbf516bb93dbdcfb73b57e84854f492
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-07-09 04:37:22 +0900

    feat(mock): ProductMockData 구조 추가

--- src/types/product.type.ts
+++ src/types/product.type.ts
@@ -8,3 +8,16 @@ export interface ProductData {
   reviewCount?: number;
   shippingInfo?: string;
 }
+
+export interface ProductMockData {
+  id: string;
+  category: string;
+  name: string;
+  imageUrl: string;
+  price: number;
+  originalPrice: number;
+  seller: string;
+  reviewCount: number;
+  shippingInfo: string;
+  badges: { text: string; color: string }[];
+}
